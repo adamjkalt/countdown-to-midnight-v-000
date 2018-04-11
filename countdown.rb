74bac9620701247d767bf421ab24fd02c63a3254
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-11 17:13:04 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,12 @@ def countdown(num)
 end
 return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+  num = 10
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+end
+return "HAPPY NEW YEAR!"
+end
