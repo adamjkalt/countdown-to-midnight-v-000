c35e5bcd05b8e761fed449a2fb76171878158827
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-11 17:13:51 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
-def countdown(countdown)
-  countdown = 10
-  while countdown > 0
-    puts "#{countdown} SECOND(S)!"
-    countdown -= 1
+def countdown(num)
+  num = 10
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
 end
 return "HAPPY NEW YEAR!"
 end
